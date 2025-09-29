18bcc2b2ab43ab7e7ad41b407b5a7961141abd1e
sofiecarel <sofiearahan@gmail.com>
2025-09-29 21:08:20 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
 import 'app_monitor.dart';
+import 'console_logger.dart';
+
+void main() {
+  print("--- Demo: AppMonitor with ConsoleLogger ---");
+  var console = ConsoleLogger();
+  var monitor1 = AppMonitor(console);
+  monitor1.startService("AuthService");
+  monitor1.stopService("AuthService");
+}
